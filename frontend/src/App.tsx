21be84d5f7b43ec0313269ae624b894c78c524e6
anarchy sano <pasindu.metaroon@gmail.com>
2024-02-05 01:00:42 +0530

    feat: initiate frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+    return (
+        <div>
+
+            <div className='cards__section'>
+
+                <h3>Welcome to Millow</h3>
+
+            </div>
+
+        </div>
+    );
+}
+
+export default App
